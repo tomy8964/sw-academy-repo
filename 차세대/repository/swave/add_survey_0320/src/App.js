7f3f24cad1d7158993b297af4803e633ce401228
tomy8964 <tomy8964@naver.com>
2023-03-20 12:41:27 +0900

    0320_survey_add

---
+++ 차세대/repository/swave/add_survey_0320/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Teammate from './component/Teammate';
+import Home from './component/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import Vision from './component/Vision';
+import About from './component/About';
+import Content from './component/Contents';
+import Survey from './component/Survey';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={ <Home />}>
+          </Route>
+          <Route path="/vision" element={<Vision />} />
+          <Route path="/teammate" element={<Teammate />} />
+          <Route path="/teammate/:member" element={<About />} />
+          <Route path="/contents" element={<Content />} />
+          <Route path="/survey" element={<Survey />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+export default App;
